0e974bd03b0c19cc2be8fc7cc47cf1e5ad45d330
Marcio Moreira <fc41972@alunos.fc.ul.pt>
2026-04-14 11:17:08 +0100

    Hello World COBOL para teste

---
+++ src/hello.cob
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, COBOL on macOS!".
+           
+           
+       
+           STOP RUN.
+
